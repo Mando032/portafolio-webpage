f117e91995a6041e092c621debe4897ae1c6d694
Mando032 <armandomendoza11111@gmail.com>
2024-06-17 23:12:26 -0700

    updated footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
    <>
       <footer>
          <ul className="links">
-            <li><a href="#github">GitHub</a></li>
-            <li><a href="#linkdIn">LinkdIn</a></li>
+            <li><a href="https://github.com/Mando032">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/mendoza-sanchez/">LinkedIn</a></li>
          </ul>
       </footer>
    </>
